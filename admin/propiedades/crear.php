78c401755e6022fc96a8a6e0d267c0b208f7e215
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-02-20 18:55:39 -0600

    Add files to make a CRUD

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,13 @@
+<?php
+    require '../../includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+        <main class="contenedor">
+            <h1>Crear</h1>
+            <a href="/S26-BienesRaices/admin/index.php" class="boton boton-verde">Volver</a>
+        </main>
+
+<?php
+    incluirTemplate('footer');
+?>
