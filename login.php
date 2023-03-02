18ae4bce2fa93b4b5ee702be16430734c91bccf2
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-02 17:06:45 -0600

    Add Front-End validation

--- login.php
+++ login.php
@@ -1,21 +1,52 @@
 <?php
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    $errores = [];
+
+    //autenticar el usuario
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        // echo '<pre>';
+        // var_dump($_POST);
+        // echo '</pre>';
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        $email = mysqli_real_escape_string($db, filter_var($email,FILTER_VALIDATE_EMAIL));
+        $password = mysqli_real_escape_string($db, $password);
+
+        if(!$email){
+            $errores[] = 'El email no es válido';
+        }
+        if(!$password){
+            $errores[] = 'El password es obligatorio';
+        }
+
+    }
+
+    //header
     require 'includes/funciones.php';
     incluirTemplate('header');
 ?>
 
-    <main class="contenedor contenido-centrado">
+    <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar Sesión</h1>
-        <form action="" class="formulario">
+
+        <?php foreach($errores as $error): ?>
+            <p class="alerta error"><?php echo $error; ?></p>
+        <?php endforeach; ?>
+        <form method="POST" class="formulario" novalidate>
             <fieldset>
                 <legend>
                     Email y Password
                 </legend>
 
                 <label for="email">E-mail</label>
-                <input type="email" placeholder="Tu Email" id="email">
+                <input type="email" name="email" placeholder="Tu Email" id="email">
 
                 <label for="password">Password</label>
-                <input type="password" placeholder="Tu constraseña" id="password">                    
+                <input type="password" name="password" placeholder="Tu constraseña" id="password">                    
             </fieldset>
 
             <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
