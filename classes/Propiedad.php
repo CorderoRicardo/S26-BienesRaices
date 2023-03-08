28eedfee33dfe9d660ddeed5d58660499a6bfffc
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-08 15:34:18 -0600

    Add a static connection to the DB

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -3,6 +3,9 @@
 namespace App;
 
 class Propiedad{
+    //Conexion a la base de datos
+    protected static $db;
+
     public $id;
     public $titulo;
     public $precio;
@@ -19,12 +22,29 @@ class Propiedad{
         $this->id = $args['id'] ?? '';
         $this->titulo = $args['titulo'] ?? '';
         $this->precio = $args['precio'] ?? '';
-        $this->imagen = $args['imagen'] ?? '';
+        $this->imagen = $args['imagen'] ?? 'imagen.jpg';
         $this->descripcion = $args['descripcion'] ?? '';
         $this->habitaciones = $args['habitaciones'] ?? '';
         $this->wc = $args['wc'] ?? '';
         $this->estacionamiento = $args['estacionamiento'] ?? '';
-        $this->creado = $args['creado'] ?? '';
+        $this->creado = date('Y/m/d');
         $this->vendedorId = $args['vendedorId'] ?? '';
     }
+
+    public function guardar(){
+    // Crear la query del INSERT
+        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedores_Id) VALUES 
+        ('$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId');";
+
+        $resultado = self::$db->query($query);
+
+    }
+
+    /**
+     * Set up the connection to a MySQL DB 
+     */
+    public static function setDB($database){
+        self::$db = $database;
+    }
+
 }
