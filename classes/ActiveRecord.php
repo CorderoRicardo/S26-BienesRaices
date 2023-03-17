32388ea32c302937eb5bb116798b8ccaa02c9dac
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-16 19:20:08 -0600

    Adjust methods some methods with 'self::' to use 'static::'

--- classes/ActiveRecord.php
+++ classes/ActiveRecord.php
@@ -3,34 +3,9 @@ namespace App;
 class ActiveRecord{
     //Conexion a la base de datos
     protected static $db;
-    protected static $columnasDB = ['id','titulo','precio','imagen','descripcion','habitaciones','wc','estacionamiento','creado','vendedorId'];
+    protected static $columnasDB = [];
     protected static $errores = [];
     protected static $tabla = '';
-
-    public $id;
-    public $titulo;
-    public $precio;
-    public $imagen;
-    public $descripcion;
-    public $habitaciones;
-    public $wc;
-    public $estacionamiento;
-    public $creado;
-    public $vendedorId;
-
-    public function __construct($args = [])
-    {
-        $this->id = $args['id'] ?? null;
-        $this->titulo = $args['titulo'] ?? '';
-        $this->precio = $args['precio'] ?? '';
-        $this->imagen = $args['imagen'] ?? '';
-        $this->descripcion = $args['descripcion'] ?? '';
-        $this->habitaciones = $args['habitaciones'] ?? '';
-        $this->wc = $args['wc'] ?? '';
-        $this->estacionamiento = $args['estacionamiento'] ?? '';
-        $this->creado = date('Y/m/d');
-        $this->vendedorId = $args['vendedorId'] ?? '';
-    }
 
     public function guardar(){
         if(!is_null($this->id)){
@@ -101,7 +76,7 @@ class ActiveRecord{
      */
     public function atributos(){
         $atributos = [];
-        foreach(self::$columnasDB as $columna){
+        foreach(static::$columnasDB as $columna){
             if($columna === 'id') continue;
             $atributos[$columna] = $this->$columna;
         }
@@ -138,43 +113,20 @@ class ActiveRecord{
     }
 
     public static function getErrores(){
-        return self::$errores;
+        return static::$errores;
     }
 
     /**
      * Fill the 'errores' array with messages to display in the UI
      */
     public function validar(){
-        if(!$this->titulo){
-            self::$errores[] = 'El Titulo es obligatorio';
-        }
-        if(!$this->precio){
-            self::$errores[] = 'El Precio es obligatorio';
-        }    
-        if(strlen($this->descripcion) < 50){
-            self::$errores[] = 'La Descripción es obligatoria y debe tener al menos 50 caracteres';
-        }    
-        if(!$this->habitaciones){
-            self::$errores[] = 'El número de Habitaciones es obligatorio';
-        }
-        if(!$this->wc){
-            self::$errores[] = 'El número de Baños es obligatorio';
-        }
-        if(!$this->estacionamiento){
-            self::$errores[] = 'El número de lugares de Estacionamiento es obligatorio';
-        }
-        if(!$this->vendedorId){
-            self::$errores[] = 'Elige un vendedor';
-        }
-        if(!$this->imagen){
-            self::$errores[] = 'La imagen es obligatoria';
-        }
-        return self::getErrores();
+        static::$errores = [];
+        return static::getErrores();
     }
 
-    /** Returns all rows of the table 'propiedades' */
+    /** Returns all rows of the respective table */
     public static function all(){
-        $query = "SELECT * FROM ".static::$tabla." ";
+        $query = "SELECT * FROM ".static::$tabla;
         $resultado = self::consultarSQL($query);
         return $resultado;
     }
@@ -193,7 +145,7 @@ class ActiveRecord{
         //Iterar los resultados
         $arreglo = [];
         while($registro = $resultado->fetch_assoc()){
-            $arreglo[] = self::crearObjeto($registro);
+            $arreglo[] = static::crearObjeto($registro);
         }
 
         $resultado->free();
@@ -203,7 +155,7 @@ class ActiveRecord{
     }
 
     protected static function crearObjeto($registro){
-        $objeto = new self;
+        $objeto = new static;
 
         foreach($registro as $key => $value){
             if(property_exists($objeto,$key)){
